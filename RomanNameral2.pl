2a7b48a53bf920d3c9c27e21b1c850eabde6546d
FIOpwK <fiopwk@outlook.com>
2018-11-29 19:29:20 -0500

    new file:   RomanNameral2.pl 	modified:   RomanNameral3.pl

--- RomanNameral2.pl
+++ RomanNameral2.pl
@@ -133,26 +133,25 @@
 use strict;
 use warnings;
 
+while (1) {
+    print "Enter a name to see its weight: \n\n";
+    $_ = <STDIN>;    # input
+    chomp $_;
+    exit 0 if ( $_ eq "" );
+    my %valuest =    # set up, input control w/ %hash value
+      ( I => 1, X => 10, V => 5, M => 1000, D => 500, C => 100, L => 50, );
 
-    while (1) {
-        print "Enter a name to see its weight: \n\n";
-        $_ = <STDIN>;    # input
-        chomp $_;
-        exit 0 if ( $_ eq "" );
-        my %valuest =    # set up, input control w/ %hash value
-          ( I => 1, X => 10, V => 5, M => 1000, D => 500, C => 100, L => 50, );
-
-        sub score {
-            my $str   = shift;
-            my $score = 0;
-            foreach my $key ( keys %valuest ) {
-                my $count = () = $str =~ /(\Q$key\E)/gi;
-                $score += $count * $valuest{$key};
-            }
-            return $score;
+    sub score {
+        my $str   = shift;
+        my $score = 0;
+        foreach my $key ( keys %valuest ) {
+            my $count = () = $str =~ /(\Q$key\E)/gi;
+            $score += $count * $valuest{$key};
         }
-        print "\n\n";
-        print " weight is: ";
-        print score "$_";    # output
-        print "\n\n";
+        return $score;
     }
+    print "\n\n";
+    print " weight is: ";
+    print score "$_";    # output
+    print "\n\n";
+}
